c61defe46690f0cf6a9e0e55229c817abb82866f
Victor Benetti <b3n3771@hotmail.com>
2025-05-26 17:42:11 -0300

    finalizado

--- src/main/java/com/devsuperior/desafio1/Desafio1Application.java
+++ src/main/java/com/devsuperior/desafio1/Desafio1Application.java
@@ -26,9 +26,21 @@ public class Desafio1Application implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		Order order = new Order(1309, 95.90, 0.0);
+
+		Order order = new Order(1034, 150.0, 20.0);
+		System.out.println("\nEXEMPLO 1");
 		System.out.println("Pedido código: "  + order.getCode());
-		System.out.println("Valor total: " + ((order.getBasic() * (1 - order.getDiscount() / 100)) + shippingService.shipment(order)));
+		System.out.println("Valor total: R$ " + orderService.total(order));
+
+		Order order2 = new Order(2282, 800.0, 10.0);
+		System.out.println("\nEXEMPLO 2");
+		System.out.println("Pedido código: "  + order2.getCode());
+		System.out.println("Valor total: R$ " + orderService.total(order2));
+
+		Order order3 = new Order(1309, 95.90, 0.0);
+		System.out.println("\nEXEMPLO 3");
+		System.out.println("Pedido código: "  + order3.getCode());
+		System.out.println("Valor total: R$ " + orderService.total(order3));
 
 	}
 
